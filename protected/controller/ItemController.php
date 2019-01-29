0c2d3b502c74d223468928446c5a5e3c0ac8e99c
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-01-29 23:53:16 +0800

    'change-from-oid-to-iid'

--- protected/controller/ItemController.php
+++ protected/controller/ItemController.php
@@ -5,12 +5,18 @@ class ItemController extends BaseController
 
     public function actionDetail()
     {
-        $oid=arg('oid');
+        $iid=arg('iid');
         $this->url="item/detail";
-        //TODO 当$oid($oid为订单的编号)为空，显示的是测试内容，后期记得改为跳转至404页面
-        if(empty($oid)){
+        //TODO 当$iid($iid为订单的编号)为空，显示的是测试内容，后期记得改为跳转至404页面
+        $user=new Model("users");
+        $item=new Model("item");
+        $item_res=$item->find(array(
+            "iid=:iid",
+            ":iid" => $iid,
+        ));
+        if(empty($iid)||empty($item_res)){
             $this->item_info = array(
-                "name" => "你好,这是个测试样例,查询物品请传入iid的值",
+                "name" => "你好,这是个测试样例,查询物品请传入正确的iid的值",
                 "create_time" => "2019/1/27 20:00", //发布时间
                 "limit_time" => "3",//借用时限
                 "count" => "1",
@@ -27,31 +33,10 @@ class ItemController extends BaseController
             );
         }
         else{
-            $order=new Model("`order`");
-            $user=new Model("users");
-            $item=new Model("item");
-            $order_res=$order->find(array(
-                "oid=:oid",
-                ":oid" => $oid,
-            ));
-            $user_res=$user->find(array(
-                "uid=:uid",
-                ":uid" => $order_res["renter_id"],
-            ));
-            $this->publisher_info = array(
-                "publisher" => $user_res["name"],
-                "publisher_credit" => $user_res["credit"], //出借者信用
-                "publisher_order_count" => "暂时空缺", //总出借笔数
-                "publisher_item_count" => "暂时空缺" //发布物品数
-            );//TODO上面两处空缺需要一些sql方面的高级操作，后续再补
-            $item_res=$item->find(array(
-                "iid=:iid",
-                ":iid" => $order_res["item_id"],
-            ));
             $this->item_info = array(
                 "name" => $item_res["name"],
                 "scode" => $item_res["scode"],
-                "owener" => $item_res["owner"],
+                //"owener" => $item_res["owner"],
                 "create_time" => $item_res["create_time"], //发布时间
                 "limit_time" => $item_res["limit_time"],//借用时限
                 "count" => $item_res["count"],
@@ -64,6 +49,17 @@ class ItemController extends BaseController
                 "order_count" => $item_res["order_count"], //出借笔数
                 "desc" => $item_res["dec"] //物品描述
             );
+            $user_res=$user->find(array(
+                "uid=:uid",
+                ":uid" => $item_res["owner"],
+            ));
+            $this->publisher_info = array(
+                "publisher" => $user_res["name"],
+                "publisher_credit" => $user_res["credit"], //出借者信用
+                "publisher_order_count" => "暂时空缺", //总出借笔数
+                "publisher_item_count" => "暂时空缺" //发布物品数
+            );//TODO上面两处空缺需要一些sql方面的高级操作，后续再补
+            
         }
         $this->title=$this->item_info["name"]." - 物品详情";
     }
