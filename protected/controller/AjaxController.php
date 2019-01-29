3f1057daf8bec4583c24e5ac9396424b303ac47b
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-01-29 14:43:53 +0800

    'add-ajax-sql-part-code'

--- protected/controller/AjaxController.php
+++ protected/controller/AjaxController.php
@@ -11,8 +11,37 @@ class AjaxController extends BaseController
         {
             ERR::Catcher(100001); //提示名称为空
         }
-        SUCCESS::Catcher("发布成功！",array(
-            'itemid'=>10000, //传回物品id
-        ));
+        else{
+            $item=new Model("item");
+            $name=arg("name");
+            $timeLimit=arg("timeLimit");
+            $location=arg("location");
+            $creditRequired=arg("creditRequired");
+            $number=arg("number");
+            $desc=arg("desc");
+            $iid=$item->create(
+                array(
+                    'iid' => "",
+                    'scode' => "1",//此处约定物品无库存的状态码为0，物品有库存为1
+                    'name' => $name,
+                    'count' => $number,
+                    "owner" => $this->userinfo['uid'],
+                    "dec" => $desc,
+                    "create_time"=> date("Y-m-d H:i:s",time()),
+                    "limit_time" => $timeLimit,
+                    "pic" => "",//等前端部分可以上传图片了再补充
+                    "location" => $location,
+                    "gcount" => "",
+                    "mcount" => "",
+                    "bcount" => "",
+                    "order_count" => "",
+                    "credit_limit" => $creditRequired,   
+                )
+            );
+            SUCCESS::Catcher("发布成功！",array(
+                'itemid'=>$iid, //传回物品id
+            ));
+        }
+        
     }
 }
