63490546f8193feb3aae4de5c44833064c028166
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-02-02 00:52:10 +0800

    'add-main-index-filter'

--- protected/controller/MainController.php
+++ protected/controller/MainController.php
@@ -8,24 +8,41 @@ class MainController extends BaseController
         
         $sort=strtolower(@arg("sort"));
         $keyword=strtolower(@arg("keyword"));
-        $page=arg("page");
+        $page=@arg("page");
+        $filter=strtolower(@arg('filter'));
         $this->args=[
             'sort' => $sort,
             'keyword' => $keyword,
             'page' => $page,
-        ];  
-        if(!empty($keyword)){
-            $keyword=str_replace("[","[[]",$keyword);
-            $keyword=str_replace("_","[_]",$keyword);
-            $keyword=str_replace("%","[%]",$keyword);
-            $conditions=[
-                "name like :keyword",
-                ":keyword" => '%'.$keyword.'%',
-            ];
+            'filter' => $filter,
+        ];
+
+        $keyword=str_replace("[","[[]",$keyword);
+        $keyword=str_replace("_","[_]",$keyword);
+        $keyword=str_replace("%","[%]",$keyword);
+        $conditions[':keyword']='%'.$keyword.'%';
+        $conditions[0]=" name like :keyword ";
+        switch($filter){
+            case 'borrowable':
+                $conditions[0]=$conditions[0].'AND scode = :scode';
+                $conditions[':scode']='1';
+                $filter='AND scode =1';
+                break;
+            case 'soldout':
+                $conditions[0]=$conditions[0].'AND scode = :scode';
+                $conditions[':scode']='-1';
+                $filter='AND scode = -1';
+                break;
+            // case 'credit':
+            //     $conditions[0]
+            //TODO 貌似又是个跨表查询,等后期再实现吧
+            case 'mine':
+                $conditions[0]=$conditions[0].'AND owner = :owner';
+                $conditions[':owner']=$this->userinfo['uid'];
+                $filter='AND owner = '.$this->userinfo['uid'];
+                break;
         }
-        else{
-            $conditions=null;
-        }
+
         $items = new Model("item");
         //TODO2   关于是否展示已出借完的物品，后期再讨论讨论吧
         if($sort==="bycount"){
@@ -33,7 +50,7 @@ class MainController extends BaseController
         }
         else if($sort==="bycredit"){
             $keyword="'%".$keyword."%'";
-            $items_res=$items->query("select item.*,users.credit from item join users on item.owner=users.uid where item.name like $keyword ORDER BY users.credit DESC;");
+            $items_res=$items->query("select item.*,users.credit from item join users on item.owner=users.uid where item.name like $keyword $filter ORDER BY users.credit DESC;");
             $page=max(1,$page);
             $items->pager($page,6,6,count($items_res));
             if(!empty($items->page)){
