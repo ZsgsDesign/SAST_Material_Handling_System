cd2d3c5e7e13f6c742a39c264bb6995038ab42e9
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-02-02 11:17:51 +0800

    'add-TODO'

--- protected/controller/AjaxController.php
+++ protected/controller/AjaxController.php
@@ -90,7 +90,7 @@ class AjaxController extends BaseController
                                     'count' => $count,
                                 )
                             );
-                            SUCCESS::Catcher("成功创建添加",array(
+                            SUCCESS::Catcher("成功添加",array(
                                 'cid' => $cid,
                             ));
                         }
