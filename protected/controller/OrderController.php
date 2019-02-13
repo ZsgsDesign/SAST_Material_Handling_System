ae262edc3708eb38a3e3c48e7f296b8c4b1c1306
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-02-13 12:30:01 +0800

    'order-selected'

--- protected/controller/OrderController.php
+++ protected/controller/OrderController.php
@@ -12,9 +12,15 @@ class OrderController extends BaseController
     public function actionCreate()
     {
         $this->url="order/create";
+        $selected=arg('item');
         $this->title="创建订单";
         $cart=new Model('cart');
-        $cart_res=$cart->query("SELECT a.*,users.real_name FROM (SELECT cart.*,item.`name`,item.scode,item.`owner`,item.location FROM cart JOIN item ON cart.item_id=item.iid) AS a JOIN users ON a.`owner`=users.uid WHERE a.scode = 1 AND a.`user`= ".$this->userinfo['uid']);
+        $sql="SELECT a.*,users.real_name FROM (SELECT cart.*,item.`name`,item.scode,item.`owner`,item.location FROM cart JOIN item ON cart.item_id=item.iid) AS a JOIN users ON a.`owner`=users.uid WHERE a.scode = 1 AND a.`user`= ".$this->userinfo['uid']." ";
+        if(!empty($selected)){
+            $items_sql=implode(" OR item_id=",$selected);
+            $sql=$sql." AND( item_id=".$items_sql.")";
+        }
+        $cart_res=$cart->query($sql);
         $total_count=array_sum(array_column($cart_res,'count'));
         $this->total_count=$total_count;
         $this->order_item=$cart_res;
