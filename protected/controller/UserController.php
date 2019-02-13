83140899400c6a6c4bc0aac6923381e23e10c164
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-02-13 20:34:45 +0800

    'fix-bug-again'

--- protected/controller/UserController.php
+++ protected/controller/UserController.php
@@ -42,5 +42,6 @@ class UserController extends BaseController
         $order=new Model('`order`');
         $order_res=$order->query("SELECT a.*,users.real_name,users.uid FROM (SELECT `order`.*,item.`name`,item.iid,item.`owner` FROM `order` JOIN item ON item.iid = `order`.item_id) as a JOIN users ON users.uid=a.`owner` WHERE a.renter_id = ".$this->userinfo['uid']);
         $this->orders=$order_res;
+        dump($this->orders);
     }
 }
