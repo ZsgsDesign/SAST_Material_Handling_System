f757bf2b576c39022371f64030abbe5df9fbce33
ChenKS <42082890+ChenKS12138@users.noreply.github.com>
2019-02-13 10:37:02 +0800

    'add-order-create'

--- protected/controller/OrderController.php
+++ protected/controller/OrderController.php
@@ -13,6 +13,11 @@ class OrderController extends BaseController
     {
         $this->url="order/create";
         $this->title="创建订单";
+        $cart=new Model('cart');
+        $cart_res=$cart->query("SELECT a.*,users.real_name FROM (SELECT cart.*,item.`name`,item.scode,item.`owner`,item.location FROM cart JOIN item ON cart.item_id=item.iid) AS a JOIN users ON a.`owner`=users.uid WHERE a.scode = 1 AND a.`user`= ".$this->userinfo['uid']);
+        $total_count=array_sum(array_column($cart_res,'count'));
+        $this->total_count=$total_count;
+        $this->order_item=$cart_res;
     }
 
 }
