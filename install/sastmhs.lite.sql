e9ea25a82e4df66fb70d90f6b7146a7accb7c22b
Cone Pi <pikanglong@gmail.com>
2019-02-28 19:03:48 +0800

    change herf of main_order

--- install/sastmhs.lite.sql
+++ install/sastmhs.lite.sql
@@ -88,17 +88,10 @@ CREATE TABLE `order`  (
   `renter_review` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '借用人评价',
   `count` bigint(20) NULL COMMENT '借用的数量',
   `rent_time` datetime NULL COMMENT '借用人取用的时间',
+  `return_time` datetime NULL COMMENT '物品实际归还的时间或订单被取消的时间',
+  `renter_checked` tinyint(20) NULL DEFAULT NULL COMMENT '用于记录借用人是否已知晓当前订单状态',
+  `owner_checked` tinyint(20) NULL COMMENT '用于记录出借人是否已知晓当前订单状态',
   `owner_review_content` varchar(255) NULL COMMENT '用于记录出借人对此订单的文字评价',
   `renter_review_content` varchar(255) NULL COMMENT '用于记录借用人对此订单的文字评价',
   PRIMARY KEY (`oid`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
-SET FOREIGN_KEY_CHECKS = 1;
-
-ALTER TABLE `sastmhs`.`order`
-ADD COLUMN `count` bigint(20) NULL COMMENT '借用的数量' AFTER `renter_review`;
-ADD COLUMN `rent_time` datetime NULL COMMENT '借用人取用的时间' AFTER `count`;
-ADD COLUMN `return_time` datetime NULL COMMENT '物品实际归还的时间或订单被取消的时间' AFTER `rent_time`;
-ADD COLUMN `renter_checked` tinyint(20) NULL DEFAULT NULL COMMENT '用于记录借用人是否已知晓当前订单状态' AFTER `return_time`,
-ADD COLUMN `owner_checked` tinyint(20) NULL COMMENT '用于记录出借人是否已知晓当前订单状态' AFTER `renter_checked`;
-
